f772c20aa4e8df8472ea30f187fe2b69363075a5
Joey <jsparidaans@gmail.com>
2022-06-29 13:43:53 +0200

    started twosum problem

---
+++ py/twosum/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        return
+
+
+if __name__ == '__main__':
+    Solution().two_sum(nums=[1, 2, 3], target=4)
