ba2d985886bc2775c206a2afe3f25afe3e9f066e
Joey <jsparidaans@gmail.com>
2022-06-29 14:16:54 +0200

    written unit tests to test against

--- py/twosum/solution_test.py
+++ py/twosum/solution_test.py
@@ -1,9 +1,20 @@
 import unittest
 
+from solution import Solution
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_first_example(self):
+        solution = Solution().two_sum(nums=[2, 7, 11, 15], target=9)
+        self.assertEqual(solution, [0, 1])
+
+    def test_second_example(self):
+        solution = Solution().two_sum(nums=[3, 2, 4], target=6)
+        self.assertEqual(solution, [1, 2])
+
+    def test_third_example(self):
+        solution = Solution().two_sum(nums=[3, 3], target=6)
+        self.assertEqual(solution, [0, 1])
 
 
 if __name__ == '__main__':
