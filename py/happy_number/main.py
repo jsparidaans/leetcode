3d74ef11247e85eb7ad54441c77a1a1596d3510d
joey sparidaans <jsparidaans@gmail.com>
2022-09-07 00:56:40 +0200

    initial setup

---
+++ py/happy_number/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solution = Solution().isHappy(19)
+    print(solution)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
