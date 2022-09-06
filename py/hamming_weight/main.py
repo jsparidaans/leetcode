661a1620cae7acedf8fe5256f89d4e397a666b6d
joey sparidaans <jsparidaans@gmail.com>
2022-09-07 00:52:04 +0200

    solution using string manipulation

--- py/hamming_weight/main.py
+++ py/hamming_weight/main.py
@@ -1,10 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        pass
+        return len(str(bin(n)).replace("0b", "").replace("0", ""))
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    solution = Solution().hammingWeight(0b01011)
+    solution = Solution().hammingWeight(0b00000000000000000000000010000000)
     print(solution)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
