e82ab78e8909b1423676f3b044b7f1f5b001810a
joey sparidaans <jsparidaans@gmail.com>
2022-09-07 00:46:13 +0200

    initial setup

---
+++ py/hamming_weight/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solution = Solution().hammingWeight(0b01011)
+    print(solution)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
